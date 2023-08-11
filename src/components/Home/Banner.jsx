6aef8152432eb2250935dedf9aea453f16af172b
Ezeora Kenechukwu Johnbosco <kencodict@gmail.com>
2023-08-11 10:01:46 +0100

    removed banner content animation

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -51,16 +51,16 @@ const Banner = () => {
 {
     slides.map(({title, subtitle, text, link, image},index) => {
 return (
-<article className={`h-[500px] w-full lg:h-screen lg:w-full  relative`} key={index}>
+<article className={`h-[400px] w-full lg:h-screen lg:w-full  relative`} key={index}>
 <div className="w-full h-full absolute left-0 right-0 bg-[rgba(0,0,0,0.5)] z-5 flex flex-col items-center justify-center ">
-   <Slide cascade={true} damping={0.5} direction='up' duration={500} fraction={1} triggerOnce={false}>
+  
    <h1 className="text-[1.5rem] md:[2rem] lg:text-[3rem] font-black p-10 border-4 rounded-br-2xl rounded-tl-2xl border-gold-100 text-[white] uppercase text-center">{title}</h1>
     <h2 className="text-[1.2rem] lg:text-[2rem] font-black rounded-tl-2xl text-[white] uppercase">{subtitle}</h2>
     <h4 className="lg:text-[1.2rem] text-[1rem] font-black rounded-tl-2xl text-[white] uppercase mb-7 max-w-[600px] text-center">{text}</h4>
-   </Slide>
-<Rotate cascade={true} damping={1} direction='up' duration={1000} fraction={1} triggerOnce={false} delay={500}>
+
+
 <a href={link} className="inline-flex items-center px-6 py-4  rounded-md font-semibold text-lg text-[white] dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 bg-gold-700 bg-white bg-opacity-70 backdrop-blur-lg ">Learn More</a>
-</Rotate>
+
 </div>
 
 <img src={image} alt="" className="h-full w-full " />
